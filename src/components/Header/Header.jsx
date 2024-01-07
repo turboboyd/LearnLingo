4a245b5ddc89cdbb377d159cfe293f34e6eed1fe
Denis Daniv <113061164+turboboyd@users.noreply.github.com>
2024-01-07 13:13:41 +0100

    Header
    Header connected, minimal styles for header.
    Opening a standard modal window by clicking a button.
    Navigation between pages in the header.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,50 @@
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { HOME_ROUTE, TEACHERS_ROUTE, FAVORITES_ROUTE } from 'utils/const';
+import css from './Header.module.css';
+import BasicModal from 'components/Modal/BasicModal';
 
 export default function Header() {
+  const [isModalOpen, setModalOpen] = useState(false);
+
+  const isModal = () => {
+    setModalOpen(!isModalOpen);
+  };
+
   return (
-    <nav>
-      <ul>
+    <header className={css.header}>
+      <Link to={HOME_ROUTE}>
+        <p>LOGO</p>
+        <p className={css.logo_title}>LearnLingo</p>
+      </Link>
+      <nav>
+        <ul className={css.list_nav}>
+          <li>
+            <Link to={HOME_ROUTE}>Home</Link>
+          </li>
+          <li>
+            <Link to={TEACHERS_ROUTE}>Teachers</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <ul className={css.list_btn}>
         <li>
-          <Link to={HOME_ROUTE}>Home</Link>
+          <button className={css.btn_login} onClick={isModal}>
+            Log in
+          </button>
         </li>
         <li>
-          <Link to={TEACHERS_ROUTE}>Teachers</Link>
+          <button className={css.btn} onClick={isModal}>
+            Registration
+          </button>
         </li>
       </ul>
-    </nav>
+      {!isModalOpen && (
+        <BasicModal isModal={isModal}>
+          <p>Your modal content goes here</p>
+        </BasicModal>
+      )}
+    </header>
   );
 }
