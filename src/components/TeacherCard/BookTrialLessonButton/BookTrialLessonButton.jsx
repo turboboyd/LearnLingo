e8a6260f857334a8f67b16513e5e6921b7cb15f4
Denis Daniv <113061164+turboboyd@users.noreply.github.com>
2024-01-31 11:04:05 +0100

    fix css

--- src/components/TeacherCard/BookTrialLessonButton/BookTrialLessonButton.jsx
+++ src/components/TeacherCard/BookTrialLessonButton/BookTrialLessonButton.jsx
@@ -1,13 +1,15 @@
 import css from './BookTrialLessonButton.module.css'
-export const BookTrialLessonButton = ({ expanded, randomStyle, openModal }) => (
+export const BookTrialLessonButton = ({ expanded, randomStyle, openModal }) =>
   expanded && (
     <button
       className={css.btn}
       type="button"
-      style={{ backgroundColor: randomStyle.btn }}
+      style={{
+        backgroundColor: randomStyle.btn,
+        '--active-color': 'red',
+      }}
       onClick={openModal}
     >
       Book trial lesson
     </button>
-  )
-);
+  );
